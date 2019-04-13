c9acd2b7b8b79e74dfd9bfee367cbfcd38e94f7b
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-04-13 13:51:59 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,5 +1,7 @@
 module Memorable
-    
+  
+    module ClassMethods
+      
   def reset_all
     self.all.clear
   end
@@ -7,5 +9,9 @@ module Memorable
   def count
     self.all.count
   end
+end 
+
+module InstanceMethods
+  
 
 end
