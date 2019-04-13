b3e0c10e44e02113e49b17e0911bfe3b77099c24
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-04-13 14:06:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,17 +2,13 @@ require 'pry'
 
 class Song
   
-  extend Memorable
-  include Paramable
+  extend Memorable::ClassMethods
+  include Paramable::InstanceMethods
   
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
-
-  def initialize
-    @@songs << self
-  end
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
